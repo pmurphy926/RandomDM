f2f984dae0c4dfc7b64f573b1c48fb7b5e26e0e1
pmurphy926 <patrick.murphy926@gmail.com>
2023-03-16 15:47:40 -0400

    initial commit

---
+++ src/components/nav.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Nav = (props) => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+        </>
+  )
+}
+
+export default Nav
